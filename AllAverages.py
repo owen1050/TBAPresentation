526d47ec4d9f1db09d8a52deedf6368c282052f5
owen1050 <owen1050@gmail.com>
2024-12-08 10:53:56 -0500

    Create AllAverages.py

---
+++ AllAverages.py
@@ -0,0 +1,38 @@
+import tbapy
+
+
+def getAverageScore(team, year):
+
+	matches = tba.team_matches(team, year = year, simple = True)
+
+	totalScore = 0
+	numberOfMatches = len(matches)
+
+	for match in matches:
+		isOnRed = team in match["alliances"]["red"]["team_keys"]
+		if(isOnRed):
+			totalScore = totalScore + match["alliances"]["red"]["score"]
+		else:
+			totalScore = totalScore + match["alliances"]["blue"]["score"]
+
+	if(numberOfMatches == 0):
+		return 0
+	return totalScore/numberOfMatches
+
+f = open("API_KEY.txt", "r")
+key = f.read()
+f.close()
+
+tba = tbapy.TBA(key)
+
+year = 2024
+teams = tba.district_teams(str(year) + "fma", keys = True)
+print("Got a list of " + str(len(teams)) + " teams")
+
+for team in teams[0:20]:
+	print(team + ":" + str(getAverageScore(team, year)))
+
+
+
+
+
