748d73179045ffdaa23fd86209838aece4a150a4
Owen Busler <owen1050@gmail.com>
2026-03-27 14:09:46 -0400

    Update analizeBackToBack.py

--- analizeBackToBack.py
+++ analizeBackToBack.py
@@ -2,23 +2,37 @@ import pickle
 
 backToBack = []
 
+backToBackYearTeams = {}
+
 with open('data.pkl', 'rb') as file:
     loaded_data = pickle.load(file)
 
-for year in loaded_data:
-    print(year)
-    for event in yeard:
-        print(event, year)
+for i in range(len(loaded_data)):
+    year = loaded_data[i]
+    teamsBTB = []
+    yearInt = 0
+    for event in year:
         yearInt = int(event[0:4])
         eventMinusYear = event[4:]
         try:
             teamsWonThis = year[event]
-            teamsWonNext = year[str(yearInt + 1) + eventMinusYear]
+            teamsWonNext = loaded_data[i+1][str(yearInt + 1) + eventMinusYear]
             for team in teamsWonThis:
                 if(team in teamsWonNext):
                     backToBack.append((team, eventMinusYear, yearInt))
+                    teamsBTB.append(team)
             #print(eventMinusYear, teamsWonThis, teamsWonNext)
         except:
             pass
+    backToBackYearTeams[yearInt] = teamsBTB
 
-print(backToBack)
+
+for year in backToBackYearTeams:
+    teamsAlreadySeen = []
+    for team in backToBackYearTeams[year]:
+        if(team in teamsAlreadySeen):
+            print(team, year)
+        teamsAlreadySeen.append(team)
+
+#print(backToBackYearTeams)
+#results of analysis https://docs.google.com/spreadsheets/d/1gpwu8wPT218e3WQY1XekahS6zwPsodvSDgcMBg6xwn4/edit?usp=sharing
